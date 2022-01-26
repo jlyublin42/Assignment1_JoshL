b49df141b6e6eceedd942c5703a17465032f74c3
jlyublin42 <jlyublin@gmail.com>
2022-01-26 12:18:46 -0500

    added sprites + animation

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 1;
-
     public void takeDamage(int damage)
     {
         health -= damage;
-
         if(health <= 0)
         {
             Destroy(gameObject);
         }
+    }
 
+    void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        if(otherObject.tag == "player")
+        {
+            Time.timeScale = 0.0f;
+
+            LoadScene("gameOver");
+        }
+    }
+    
+    void LoadScene(string gameOver)
+    {
+        SceneManager.LoadScene(gameOver);
     }
 }
